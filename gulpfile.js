57dae9fa7d0509f039d3c1e1a51a6e0527ea7f07
johnlarson <john.robert.larson@gmail.com>
2017-01-26 15:08:58 -0700

    I have all my gulp tasks named

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,100 @@
-#!/usr/bin/env node
+const app = require('../app');
+const debug = require('debug')('stn:server');
+const http = require('http');
+const gulp = require('gulp');
 
-/**
- * Module dependencies.
- */
+gulp.task('default', ['dev']);
 
-var app = require('../app');
-var debug = require('debug')('stn:server');
-var http = require('http');
+gulp.task('dev', ['build-dev', 'dev-server']);
+
+gulp.task('build-dev', ['copy-dev', 'bundle-dev', 'babel-server-dev']);
+
+gulp.task('copy-dev', () => {
+	return gulp.src(['src/**/*', 'src/**/.*'])
+		.pipe(watch(['src/**/*', 'src/**/.*']))
+		.pipe(exclude('**/*.js'))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('bundle-dev', callback => {
+	let jsPaths = getClientJsPaths();
+	let entryPoints = {};
+	for(let i = 0; i < jsPaths.length; i++) {
+		let src = jsPaths[i];
+		let dest = src.replace('src/', '');
+		entryPoints[dest] = src;
+	}
+
+	let compiler = webpack({
+		entry: entryPoints,
+		output: {
+			path: path.join(__dirname, 'dist'),
+			filename: '[name]',
+			publicPath: `http://localhost:${port}/`
+		},
+		module: {
+			loaders: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+					query: {
+						presets: ['es2015', 'react']
+					}
+				},
+			]
+		},
+	},
+	(err, stats) => {
+		gutil.log('[webpack]', stats.toString({}));
+		if(err) callback();
+	});
+});
+
+gulp.task('babel-server-dev', () => {
+
+});
+
+gulp.task('dev-server', () => {
+
+});
+
+function getClientJsPaths() {
+	let paths = new Set();
+	let allFiles = wrench.readdirSyncRecursive('src');
+	let htmlFiles = allFiles.filter((file) => {
+		return file.match('\.html$');
+	});
+	htmlFiles.map((htmlFile) => {
+		let currentFilesPaths = getJsPathsFrom(htmlFile);
+		currentFilesPaths.map((jsFile) => {
+			paths.add(jsFile);
+		});
+	});
+	return Array.from(paths);
+}
+
+function getJsPathsFrom(file) {
+	let result = [];
+	let filePath = path.join('src', file);
+	let htmlContents = fs.readFileSync(filePath, 'utf8');
+	let $ = cheerio.load(htmlContents);
+	let $scripts = $('script');
+	for(let i = 0; i < $scripts.length; i++) {
+		let scriptTag = $scripts[i];
+		let jsFilePath = $(scriptTag).attr('src');
+		let joinedPath = resolveScriptPath(filePath, jsFilePath);
+		let withForwardSlash = joinedPath.split('\\').join('/');
+		let startWithDot = './' + withForwardSlash;
+		result.push(startWithDot);
+	}
+	return result;
+}
+
+function resolveScriptPath(htmlPath, jsPath) {
+	let dirName = path.dirname(htmlPath);
+	return path.join(dirName, jsPath);
+}
 
 /**
  * Get port from environment and store in Express.
@@ -34,19 +122,19 @@ server.on('listening', onListening);
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+	var port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+	if (port >= 0) {
+		// port number
+		return port;
+	}
 
-  return false;
+	return false;
 }
 
 /**
@@ -54,27 +142,27 @@ function normalizePort(val) {
  */
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+	var bind = typeof port === 'string'
+		? 'Pipe ' + port
+		: 'Port ' + port;
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use');
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
 }
 
 /**
@@ -82,9 +170,9 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+	var addr = server.address();
+	var bind = typeof addr === 'string'
+		? 'pipe ' + addr
+		: 'port ' + addr.port;
+	debug('Listening on ' + bind);
 }
